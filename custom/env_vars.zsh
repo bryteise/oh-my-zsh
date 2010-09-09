af39cbf3f0948b5ea91f063bc505574f22808912
William Douglas <william.r.douglas@gmail.com>
2010-09-08 23:29:21 -0700

    updated zshrc to update path before calling customize, updated path to reflect this and change syntax to prefered zsh style

--- custom/env_vars.zsh
+++ custom/env_vars.zsh
@@ -1,5 +1,5 @@
 # Set editor for emacs
-export EDITOR='emacsclient -t -a ""'
-export VISUAL='emacsclient -t -a ""'
+export EDITOR="emacsclient -t -a ''"
+export VISUAL="emacsclient -t -a ''"
 # export path to include personal bin
-export PATH=$PATH:/home/wdouglas/bin
+path=(~/bin $path)
