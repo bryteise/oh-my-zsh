41595542511d95953dde72646206b80b8cf08477
bryteise <william.r.douglas@gmail.com>
2010-09-12 10:44:39 -0700

    updated sshpkey function to send to an optional port

--- custom/function.zsh
+++ custom/function.zsh
@@ -1,5 +1,10 @@
 # copy pubkey to server over ssh and add to authorized_keys
 function sshpkey() {
-        echo "Sending public key to $1";
-        cat ~/.ssh/id_rsa.pub | ssh $1 "cat - >> ~/.ssh/authorized_keys2"
+    if [[ $2 =~ [0-9]+ ]]; then
+        echo "Sending public key to $1 on port $2";
+        cat ~/.ssh/id_rsa.pub | ssh $1 -p $2 "cat - >> ~/.ssh/authorized_keys2"
+    else
+	echo "sending public key to $1 on port 22";
+	cat ~/.ssh/id_rsa.pub | ssh $1 "cat - >> ~/.ssh/authorized_keys2"
+    fi
 }
