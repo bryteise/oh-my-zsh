0c3663e80c492fbc27f06cf34faa4d8eeeb699e4
William Douglas <william.douglas@linux.intel.com>
2011-09-14 09:53:40 -0700

    Revert "disable auto update"
    This reverts commit 4f838915050f5b7afa2abf9dcfc9ceab533f15a7.

--- custom/env_vars.zsh
+++ custom/env_vars.zsh
@@ -1,6 +1,5 @@
 # Set editor for emacs
 export EDITOR="emacsclient -t -a ''"
 export VISUAL="emacsclient -t -a ''"
-export DISABLE_AUTO_UPDATE="true"
 # export path to include personal bin
 path=(~/bin /usr/local/bin $path)
