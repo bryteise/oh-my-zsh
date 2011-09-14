d4b3ce48c5a02037a1bd3ea24cf65289e2d69f9f
William Douglas <william.douglas@intel.com>
2011-09-14 09:31:08 -0700

    disable auto update
    Signed-off-by: William Douglas <william.douglas@intel.com>

--- custom/env_vars.zsh
+++ custom/env_vars.zsh
@@ -1,5 +1,6 @@
 # Set editor for emacs
 export EDITOR="emacsclient -t -a ''"
 export VISUAL="emacsclient -t -a ''"
+export DISABLE_AUTO_UPDATE="true"
 # export path to include personal bin
 path=(~/bin /usr/local/bin $path)
