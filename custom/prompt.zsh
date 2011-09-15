8d3a8d5e86ac6556166892bec30af3bcbeef7b26
William Douglas <william.douglas@linux.intel.com>
2011-09-15 19:23:24 +0000

    fix tramp by just using bash
    Signed-off-by: William Douglas <william.douglas@linux.intel.com>

--- custom/prompt.zsh
+++ custom/prompt.zsh
@@ -1,11 +1,4 @@
 # prompts
 if [[ $TERM == "dumb" ]]; then	# in emacs
-    PS1='% '
-    # for tramp to not hang, need the following. cf:
-    # http://www.emacswiki.org/emacs/TrampMode
-    unsetopt zle
-    unsetopt prompt_cr
-    unsetopt prompt_subst
-    unfunction precmd
-    unfunction preexec
+    exec /bin/bash
 fi
