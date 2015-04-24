2f34927999acc11d4a4310bbb3a8f4e093b9125b
William Douglas <william.douglas@intel.com>
2015-04-24 09:54:50 -0700

    Enable 256 color xfce4-terminal

--- custom/env_vars.zsh
+++ custom/env_vars.zsh
@@ -3,3 +3,8 @@ export EDITOR="emacsclient -t -a ''"
 export VISUAL="emacsclient -t -a ''"
 # export path to include personal bin
 path=(~/bin /usr/local/bin $path)
+# Get terminal color settings right
+if [ -e /usr/share/terminfo/x/xterm-256color ] && \
+       [ "$COLORTERM" = "xfce4-terminal" ]; then
+    export TERM=xterm-256color
+fi
