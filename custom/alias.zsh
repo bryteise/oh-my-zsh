15e8f27e152a09ab3a981ba7132230abace2c87e
William Douglas <william.douglas@intel.com>
2017-04-14 22:03:28 +0000

    Add E for sudo edit with emacsclient

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -1,6 +1,7 @@
 # for use with emacs in daemon mode
 alias e="emacsclient -c -a '' -n"
 alias et="emacsclient -t -a ''"
+alias E="SUDO_EDITOR=\"emacsclient -t -a emacs\" sudoedit"
 
 # look for examples in man pages
 alias lemp="LESS='+/^EXAMPLE' man "
