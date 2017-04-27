d523bc96b86d7c704821ef7fa5d7c301785cf31a
William Douglas <william.r.douglas@gmail.com>
2017-04-26 20:43:04 -0700

    Update alias with modern usage
    Remove a number of outdated aliases, also add youtube-dl aliases.

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -3,28 +3,14 @@ alias e="emacsclient -c -a '' -n"
 alias et="emacsclient -t -a ''"
 alias E="SUDO_EDITOR=\"emacsclient -t -a emacs\" sudoedit"
 
-# look for examples in man pages
-alias lemp="LESS='+/^EXAMPLE' man "
-
 # make tmux a little less verbose
 alias tmuxa="tmux attach -t"
 alias tmuxn="tmux new -s"
 alias tmuxl="tmux list-session"
 
-# indent settings for the linux kernel
-alias indentk="indent -kr -i8 -ts8 -sob -l80 -ss -bs -psl "
-
-# ssh to internal network
-alias issh="ssh -F /etc/ssh/ssh_config"
-
-# scp to internal network
-alias iscp="scp -F /etc/ssh/ssh_config"
-
-# monitor dmesg for updates
-alias wdmesg="watch 'dmesg | tail -20'"
-
-# extract files
-alias ex=extract_archive
-
 # git to g
 alias g=git
+
+# youtube-dl
+alias ydv="youtube-dl --format mp4 --restrict-filenames -o '%(title)s.%(ext)s'"
+alias yds="youtube-dl --audio-format aac -x --restrict-filenames -o '%(title)s.%(ext)s'"
