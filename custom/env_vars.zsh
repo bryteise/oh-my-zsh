a4abf10b86f18e4a36cecab54e13e71b463ef50a
William Douglas <william.r.douglas@gmail.com>
2021-08-10 15:06:03 -0700

    Add cargo bin path and remove old rust src path

--- custom/env_vars.zsh
+++ custom/env_vars.zsh
@@ -2,7 +2,7 @@
 export EDITOR="emacsclient -t -a ''"
 export VISUAL="emacsclient -t -a ''"
 # export path to include personal bin
-path=(~/bin /usr/local/bin $path)
+path=(~/bin /usr/local/bin ~/.cargo/bin $path)
 # Get terminal color settings right
 if [ -e /usr/share/terminfo/x/xterm-256color ] && \
        [ "$TERM" = "xterm" ]; then
@@ -13,7 +13,5 @@ if [ -e /usr/share/terminfo/s/screen-256color ] && \
        [ "$TERM" = "screen" ]; then
     export TERM=screen-256color
 fi
-# Set rust source path for racer
-export RUST_SRC_PATH=~/src/rust/src
 # Add additonal man page locations
 export MANPATH=/usr/share/man:$MANPATH
